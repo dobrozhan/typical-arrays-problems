416723c248c4faf347c7e0619152936656af1f7c
keep_chr <dobrozhan.a@gmail.com>
2020-02-22 22:07:15 +0200

    init: upload task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (typeof array !== 'undefined' && array.length > 0) {
+    return Math.min(...array);
+  } else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (typeof array !== 'undefined' && array.length > 0) {
+    return Math.max(...array);
+  } else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (typeof array !== 'undefined' && array.length > 0) {
+    let count = array.length;
+    let sum = array.reduce((previous, current) => current += previous);
+    let avg = sum / count;
+    return avg;
+  } else {
+    return 0;
+  }
 }
